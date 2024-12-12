b74e58701fce51f57cd07556a64c75b5165e2e50
0jc1 <epicdevelopers26@gmail.com>
2024-12-11 19:53:48 -0500

    codebase change

--- backend/cointracker/accounts/apps.py
+++ backend/cointracker/accounts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CointrackerConfig(AppConfig):
+class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cointracker'
+    name = 'accounts'
--- backend/cointracker/cointracker/urls.py
+++ backend/cointracker/cointracker/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for backend project.
+URL configuration for cointracker project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from accounts.views import index_view  # Import your view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index_view, name='index'),  # Root URL displays index.html
+
 ]
