ea714723812b5a3c16becdcac94e676bedaa900c
0jc1 <epicdevelopers26@gmail.com>
2024-12-30 00:02:31 -0500

    w

--- cointracker/settings.py
+++ cointracker/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 import os
 
 # heroku config:set IS_HEROKU=1
-IS_HEROKU = os.environ.get('IS_HEROKU', None)
+IS_HEROKU = os.environ.get('IS_HEROKU', False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
